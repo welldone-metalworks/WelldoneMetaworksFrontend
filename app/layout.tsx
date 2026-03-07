113755cda764fd8dbd0faacada9a5b2e83e4726e
chiragparihar7 <chiragparihar118@gmail.com>
2026-03-07 17:44:10 +0530

    Updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,18 @@
 import "./globals.css";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import React from "react";
 
 export const metadata = {
   title: "Welldone Metalworks",
   description: "Custom steel and metal fabrication services",
 };
 
-export default function RootLayout({ children }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
       <body>
