3d7fd30beb6894855312c0ac26d0ab8f128fecb9
chiragparihar7 <chiragparihar118@gmail.com>
2026-03-28 16:00:32 +0530

    Update Added Google anlaytics

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,6 @@
 import "./globals.css";
 import LayoutWrapper from "../components/LayoutWrapper";
+import Script from "next/script"; // 👈 ADD THIS
 
 export const metadata = {
   metadataBase: new URL("https://welldone-metalworks.in"),
@@ -11,6 +12,22 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        {/* 🔥 Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-5S7MZ2457H"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-5S7MZ2457H');
+          `}
+        </Script>
+      </head>
+
       <body>
         <LayoutWrapper>{children}</LayoutWrapper>
       </body>
